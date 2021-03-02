624f1af6f1e0f6748bafced7f59a85b346b41b0b
Zak <zak.rodionov@ya.ru>
2021-03-02 16:51:47 +0300

    setup flipper

---
+++ app/src/main/java/com/zakrodionov/practicalapp/App.kt
@@ -0,0 +1,30 @@
+package com.zakrodionov.practicalapp
+
+import android.app.Application
+import com.facebook.flipper.android.AndroidFlipperClient
+import com.facebook.flipper.android.utils.FlipperUtils
+import com.facebook.flipper.core.FlipperClient
+import com.facebook.flipper.plugins.inspector.DescriptorMapping
+import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin
+import com.facebook.flipper.plugins.network.NetworkFlipperPlugin
+import com.facebook.soloader.SoLoader
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        setupFlipper()
+    }
+
+    private fun setupFlipper() {
+        SoLoader.init(this, false)
+
+        if (BuildConfig.DEBUG && FlipperUtils.shouldEnableFlipper(this)) {
+            val client: FlipperClient = AndroidFlipperClient.getInstance(this)
+            client.addPlugin(NetworkFlipperPlugin()) // Todo add to okhttp
+            client.addPlugin(InspectorFlipperPlugin(this, DescriptorMapping.withDefaults()))
+            client.start()
+        }
+    }
+}
