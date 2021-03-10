028c644ba274d75d1549e0ef806a005a2a9fce8d
Zak <zak.rodionov@ya.ru>
2021-03-10 11:47:34 +0300

    clean

--- app/src/main/java/com/zakrodionov/practicalapp/MainActivity.kt
+++ app/src/main/java/com/zakrodionov/practicalapp/MainActivity.kt
@@ -3,8 +3,7 @@ package com.zakrodionov.practicalapp
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() { //sdfsdfsdfsdfs dffsd fsdfsd fsfdddddxc
-    val a="sdfsdfsdfsdfs dffsd fsdfsd fsfsdfsdfsdfsdfs dffsd fsdfsd fsfsdfsdfsdfsdfs dffsd fsdfsd fsfsdfsdfsdfsdfs"
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
