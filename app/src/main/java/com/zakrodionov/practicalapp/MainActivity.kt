7ebacbdcaf04b7801632187c8ab3c9e7e5a97f8d
Zak <zak.rodionov@ya.ru>
2021-03-10 11:30:26 +0300

    trigger

--- app/src/main/java/com/zakrodionov/practicalapp/MainActivity.kt
+++ app/src/main/java/com/zakrodionov/practicalapp/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() { //sdfsdfsdfsdfs dffsd fsdfsd fsfdddddxc
-    val a="sdfsdfsdfsdfs dffsd fsdfsd fsfsdfsdfsdfsdfs dffsd fsdfsd fsfsdfsdfsdfsdfs dffsd fsdfsd fsfsdfsdfsdfsdfs dffsd fsdfsd fsfsdfsdfsdfsdfs dffsd fsdfsd fsfsdfsdfsdfsdfs dffsd fsdfsd fsfsdfsdfsdfsdfs dffsd fsdfsd fsfsdfsdfsdfsdfs dffsd fsdfsd fsfsdfsdfsdfsdfs dffsd fsdfsd fsfsdfsdfsdfsdfs dffsd fsdfsd fsfsdfsdfsdfsdfs dffsd fsdfsd fsfsdfsdfsdfsdfs dffsd fsdfsd fsfsdfsdfsdfsdfs dffsd fsdfsd fsfsdfsdfsdfsdfs dffsd fsdfsd fsfsdfsdfsdfsdfs dffsd fsdfsd fsfsdfsdfsdfsdfs dffsd fsdfsd fsf"
+    val a="sdfsdfsdfsdfs dffsd fsdfsd fsfsdfsdfsdfsdfs dffsd fsdfsd fsfsdfsdfsdfsdfs dffsd fsdfsd fsfsdfsdfsdfsdfs"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
