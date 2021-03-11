3edabcfac1e827862dd490ae33b946c9cce642af
Zak <zak.rodionov@ya.ru>
2021-03-11 13:49:58 +0300

    up flipper version

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -4,5 +4,5 @@ object Versions {
     const val detektVersion = "1.16.0-RC2"
     const val ktlintVersion = "10.0.0"
 
-    const val flipperVersion = "0.76.0"
+    const val flipperVersion = "0.79.1"
 }
