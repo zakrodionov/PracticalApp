c385b6ef548e00976b6cf793e7b8c40343bf742d
Zak <zak.rodionov@ya.ru>
2021-03-29 12:23:39 +0300

    add ResourceExtensions.kt

--- common/src/main/java/com/zakrodionov/common/extensions/ContextExtensions.kt
+++ common/src/main/java/com/zakrodionov/common/extensions/ContextExtensions.kt
@@ -6,18 +6,11 @@ import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
 import android.content.pm.PackageManager
-import android.content.res.ColorStateList
 import android.content.res.Configuration
 import android.net.ConnectivityManager
 import android.net.NetworkInfo
 import android.widget.Toast
-import androidx.annotation.AttrRes
-import androidx.annotation.ColorInt
-import androidx.annotation.ColorRes
-import androidx.annotation.DrawableRes
-import androidx.appcompat.content.res.AppCompatResources
 import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 
 val Context.networkInfo: NetworkInfo?
     get() = (getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager).activeNetworkInfo
@@ -39,22 +32,6 @@ fun Context.permissionGranted(permissionName: String) =
 
 fun Context.toast(message: String?, length: Int = Toast.LENGTH_SHORT) =
     Toast.makeText(this, message, length).show()
-
-fun Context.getCompatColor(@ColorRes color: Int) = ContextCompat.getColor(this, color)
-
-fun Context.getCompatDrawable(@DrawableRes drawable: Int) =
-    AppCompatResources.getDrawable(this, drawable)
-
-fun Context.getCompatColorStateList(@ColorRes color: Int): ColorStateList =
-    AppCompatResources.getColorStateList(this, color)
-
-@ColorInt
-fun Context.getColorFromAttr(@AttrRes attrColor: Int): Int {
-    val typedArray = theme.obtainStyledAttributes(intArrayOf(attrColor))
-    val textColor = typedArray.getColor(0, 0)
-    typedArray.recycle()
-    return textColor
-}
 
 fun Context.copyToClipboard(text: CharSequence, label: String) {
     val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
---
+++ common/src/main/java/com/zakrodionov/common/extensions/ResourceExtensions.kt
@@ -0,0 +1,51 @@
+package com.zakrodionov.common.extensions
+
+import android.content.Context
+import android.content.res.ColorStateList
+import android.graphics.drawable.Drawable
+import androidx.annotation.AttrRes
+import androidx.annotation.ColorInt
+import androidx.annotation.ColorRes
+import androidx.annotation.DrawableRes
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.content.ContextCompat
+import androidx.core.content.res.use
+
+/**
+ * Получение цвета из аттрибута или [ColorRes]
+ */
+@ColorInt
+fun Context.getColorFromAnyRes(someRes: Int): Int {
+    return when (resources.getResourceTypeName(someRes)) {
+        "attr" -> getAttrColor(someRes)
+        else -> getCompatColor(someRes)
+    }
+}
+
+/**
+ * Получение drawable из аттрибута или [DrawableRes]
+ */
+fun Context.getDrawableFromAnyRes(someRes: Int): Drawable? {
+    return when (resources.getResourceTypeName(someRes)) {
+        "attr" -> getAttrDrawable(someRes)
+        else -> getCompatDrawable(someRes)
+    }
+}
+
+@ColorInt
+fun Context.getAttrColor(@AttrRes attrColorId: Int): Int = obtainStyledAttributes(intArrayOf(attrColorId)).use {
+    it.getColor(0, 0)
+}
+
+fun Context.getAttrDrawable(@AttrRes attrDrawableId: Int): Drawable? =
+    obtainStyledAttributes(intArrayOf(attrDrawableId)).use {
+        it.getDrawable(0)
+    }
+
+fun Context.getCompatColor(@ColorRes color: Int) = ContextCompat.getColor(this, color)
+
+fun Context.getCompatDrawable(@DrawableRes drawable: Int) =
+    AppCompatResources.getDrawable(this, drawable)
+
+fun Context.getCompatColorStateList(@ColorRes color: Int): ColorStateList =
+    AppCompatResources.getColorStateList(this, color)
