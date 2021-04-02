3b3dee1098a1c295c8917778e04ac48d98ca0267
Zak <zak.rodionov@ya.ru>
2021-04-02 10:35:10 +0300

    add base bottom sheet dialog

---
+++ app/src/main/java/com/zakrodionov/practicalapp/app/core/BaseBottomSheetDialogFragment.kt
@@ -0,0 +1,96 @@
+package com.zakrodionov.practicalapp.app.core
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.annotation.CallSuper
+import androidx.annotation.LayoutRes
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.addRepeatingJob
+import androidx.viewbinding.ViewBinding
+import com.google.android.material.snackbar.Snackbar
+import com.zakrodionov.common.dialogs.CommonDialog.Companion.TAG_COMMON_DIALOG
+import com.zakrodionov.common.dialogs.FixedBottomSheetDialogFragment
+import com.zakrodionov.common.extensions.getCompatColor
+import com.zakrodionov.common.extensions.hideKeyboard
+import com.zakrodionov.common.extensions.setStatusBarColor
+import com.zakrodionov.common.extensions.setStatusBarLightMode
+import com.zakrodionov.common.extensions.showDialog
+import com.zakrodionov.common.extensions.showSnackbar
+import com.zakrodionov.common.extensions.showToast
+import com.zakrodionov.common.ui.ShowAction
+import com.zakrodionov.practicalapp.R
+import kotlinx.coroutines.flow.collect
+
+abstract class BaseBottomSheetDialogFragment<STATE : Any, SIDE_EFFECT : Any>(@LayoutRes contentLayoutId: Int) :
+    FixedBottomSheetDialogFragment(contentLayoutId) {
+
+    abstract val viewModel: BaseViewModel<STATE, SIDE_EFFECT>
+    abstract val binding: ViewBinding
+
+    open val statusBarColor = R.color.color_statusbar
+    open val statusBarLightMode = false
+
+    private var snackBar: Snackbar? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        activity?.setStatusBarColor(requireContext().getCompatColor(statusBarColor))
+        activity?.setStatusBarLightMode(statusBarLightMode)
+    }
+
+    @CallSuper
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupViews(view, savedInstanceState)
+
+        viewLifecycleOwner.addRepeatingJob(Lifecycle.State.STARTED) {
+            viewModel.container.sideEffectFlow.collect { sideEffect(it) }
+        }
+
+        viewLifecycleOwner.addRepeatingJob(Lifecycle.State.STARTED) {
+            viewModel.container.stateFlow.collect { render(it) }
+        }
+    }
+
+    override fun onDestroyView() {
+        clearViews()
+        super.onDestroyView()
+    }
+
+    open fun setupViews(view: View, savedInstanceState: Bundle?) = Unit
+
+    open fun clearViews() = Unit
+
+    abstract fun sideEffect(event: SIDE_EFFECT)
+
+    abstract fun render(state: STATE)
+
+    open fun handleShowAction(showAction: ShowAction) {
+        when (showAction) {
+            is ShowAction.ShowDialog -> {
+                showDialog(
+                    message = showAction.message.getText(this),
+                    fragmentManager = childFragmentManager,
+                    tag = TAG_COMMON_DIALOG
+                )
+            }
+            is ShowAction.ShowToast -> {
+                showToast(showAction.message.getText(this), Toast.LENGTH_LONG)
+            }
+            is ShowAction.ShowSnackbar -> {
+                view?.let {
+                    snackBar?.dismiss()
+                    snackBar = showSnackbar(it, showAction.message.getText(this))
+                }
+            }
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        view?.hideKeyboard()
+    }
+
+    open fun loadData() {}
+}
