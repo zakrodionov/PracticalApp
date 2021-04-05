9fc99d2495bb28ea36a9110bca644529593535ee
Zak <zak.rodionov@ya.ru>
2021-04-05 10:27:20 +0300

    refactor Result.kt add method then

--- app/src/main/java/com/zakrodionov/practicalapp/app/core/Result.kt
+++ app/src/main/java/com/zakrodionov/practicalapp/app/core/Result.kt
@@ -1,8 +1,8 @@
 package com.zakrodionov.practicalapp.app.core
 
-sealed class Result<T : Any> {
-    data class Success<T : Any>(val data: T) : Result<T>()
-    data class Failure<T : Any>(val baseError: BaseError) : Result<T>()
+sealed class Result<out T> {
+    data class Success<T>(val data: T) : Result<T>()
+    data class Failure(val baseError: BaseError) : Result<Nothing>()
 
     fun exceptionOrNull(): BaseError? =
         when (this) {
@@ -11,12 +11,18 @@ sealed class Result<T : Any> {
         }
 }
 
-inline fun <T : Any> Result<T>.onFailure(action: (baseError: BaseError) -> Unit): Result<T> {
+inline fun <T> Result<T>.onFailure(action: (baseError: BaseError) -> Unit): Result<T> {
     exceptionOrNull()?.let { action(it) }
     return this
 }
 
-inline fun <T : Any> Result<T>.onSuccess(action: (value: T) -> Unit): Result<T> {
+inline fun <T> Result<T>.onSuccess(action: (value: T) -> Unit): Result<T> {
     if (this is Result.Success) action(data)
     return this
 }
+
+suspend fun <INPUT : Any, OUTPUT : Any> Result<INPUT>.then(block: suspend (INPUT) -> Result<OUTPUT>): Result<OUTPUT> =
+    when (this) {
+        is Result.Success -> block.invoke(data)
+        is Result.Failure -> Result.Failure(baseError)
+    }
