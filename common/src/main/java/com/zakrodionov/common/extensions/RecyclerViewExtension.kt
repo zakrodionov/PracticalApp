e926333565121eb51a91f1e7f62fde72d01721a3
Zak <zak.rodionov@ya.ru>
2021-04-05 16:05:28 +0300

    add stableId param to ext

--- common/src/main/java/com/zakrodionov/common/extensions/RecyclerViewExtension.kt
+++ common/src/main/java/com/zakrodionov/common/extensions/RecyclerViewExtension.kt
@@ -3,20 +3,18 @@ package com.zakrodionov.common.extensions
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.SimpleItemAnimator
-import com.hannesdorfmann.adapterdelegates4.AbsDelegationAdapter
 
-fun <T> AbsDelegationAdapter<T>.setData(data: T) {
-    items = data
-    notifyDataSetChanged()
-}
-
+@Suppress("LongParameterList")
 fun <T : RecyclerView.ViewHolder> RecyclerView.setup(
     adapter: RecyclerView.Adapter<T>,
     layoutManager: RecyclerView.LayoutManager = LinearLayoutManager(context),
     disableChangeAnimation: Boolean = false,
     disableAllAnimation: Boolean = false,
-    clipToPadding: Boolean = false
+    clipToPadding: Boolean = false,
+    hasStableId: Boolean = true
 ) {
+    adapter.setHasStableIds(hasStableId)
+
     this.layoutManager = layoutManager
     this.adapter = adapter
     this.clipToPadding = clipToPadding
