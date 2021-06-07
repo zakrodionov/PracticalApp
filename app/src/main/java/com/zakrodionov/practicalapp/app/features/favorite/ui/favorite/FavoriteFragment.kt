d14df68a75e5e8eaaf9897f345b7e39591ca4a0c
Zak <zak.rodionov@ya.ru>
2021-06-07 17:21:01 +0300

    add dialog animation sample

--- app/src/main/java/com/zakrodionov/practicalapp/app/features/favorite/ui/favorite/FavoriteFragment.kt
+++ app/src/main/java/com/zakrodionov/practicalapp/app/features/favorite/ui/favorite/FavoriteFragment.kt
@@ -1,9 +1,12 @@
 package com.zakrodionov.practicalapp.app.features.favorite.ui.favorite
 
+import android.app.Dialog
 import android.os.Bundle
 import android.view.View
 import androidx.core.os.bundleOf
+import androidx.fragment.app.DialogFragment
 import by.kirich1409.viewbindingdelegate.viewBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.zakrodionov.common.core.TextResource
 import com.zakrodionov.common.dialogs.FixedBottomSheetDialogFragment
 import com.zakrodionov.common.dialogs.NumberPickerDialog
@@ -42,6 +45,9 @@ class FavoriteFragment : StubFragment(R.layout.fragment_favorite) {
                 theme = R.style.AlertDialog_Theme_RedButtons
             )
         }
+        binding.tvShowAnimatedDialog.setOnClickListener {
+            AnimatedDialog().showIfNotAlreadyShown(childFragmentManager, "AnimatedDialog")
+        }
         binding.tvShowBottomDialog.setOnClickListener {
             BottomDialog.newInstance(false).showIfNotAlreadyShown(childFragmentManager, "BottomDialog")
         }
@@ -65,5 +71,16 @@ class BottomDialog : FixedBottomSheetDialogFragment(R.layout.fragment_favorite) 
         fun newInstance(isFullScreen: Boolean): BottomDialog = BottomDialog().apply {
             arguments = bundleOf(ARG_IS_FULL_SCREEN to isFullScreen)
         }
+    }
+}
+
+class AnimatedDialog : DialogFragment() {
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return MaterialAlertDialogBuilder(requireContext(), R.style.AlertDialog_Theme)
+            .setView(R.layout.fragment_email)
+            .create()
+            .apply {
+                window?.setWindowAnimations(R.style.DialogAnimation)
+            }
     }
 }
