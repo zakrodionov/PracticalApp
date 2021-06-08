70d702c00168fb4055b8360e6ef93b6bc2aa4f10
Zak <zak.rodionov@ya.ru>
2021-06-08 14:35:28 +0300

    cleanup rv ext

--- common/src/main/java/com/zakrodionov/common/extensions/RecyclerViewExtension.kt
+++ common/src/main/java/com/zakrodionov/common/extensions/RecyclerViewExtension.kt
@@ -4,29 +4,34 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.SimpleItemAnimator
 import com.hannesdorfmann.adapterdelegates4.AbsDelegationAdapter
+import com.zakrodionov.common.extensions.RecyclerViewItemAnimation.ALL_ANIMATIONS
+import com.zakrodionov.common.extensions.RecyclerViewItemAnimation.DISABLE_ALL_ANIMATIONS
+import com.zakrodionov.common.extensions.RecyclerViewItemAnimation.DISABLE_CHANGE_ANIMATIONS
 
 fun <T> AbsDelegationAdapter<T>.setData(data: T) {
     items = data
     notifyDataSetChanged()
 }
 
-@Suppress("LongParameterList")
+enum class RecyclerViewItemAnimation {
+    ALL_ANIMATIONS,
+    DISABLE_CHANGE_ANIMATIONS,
+    DISABLE_ALL_ANIMATIONS
+}
+
 fun <T : RecyclerView.ViewHolder> RecyclerView.setup(
     adapter: RecyclerView.Adapter<T>,
     layoutManager: RecyclerView.LayoutManager = LinearLayoutManager(context),
-    disableChangeAnimation: Boolean = false,
-    disableAllAnimation: Boolean = false,
-    clipToPadding: Boolean = false
+    itemAnimation: RecyclerViewItemAnimation = ALL_ANIMATIONS,
+    clipToPadding: Boolean = false,
 ) {
     this.layoutManager = layoutManager
     this.adapter = adapter
     this.clipToPadding = clipToPadding
 
-    if (disableChangeAnimation) {
-        (itemAnimator as? SimpleItemAnimator)?.supportsChangeAnimations = false
-    }
-
-    if (disableAllAnimation) {
-        itemAnimator = null
+    when (itemAnimation) {
+        ALL_ANIMATIONS -> Unit
+        DISABLE_CHANGE_ANIMATIONS -> (itemAnimator as? SimpleItemAnimator)?.supportsChangeAnimations = false
+        DISABLE_ALL_ANIMATIONS -> itemAnimator = null
     }
 }
