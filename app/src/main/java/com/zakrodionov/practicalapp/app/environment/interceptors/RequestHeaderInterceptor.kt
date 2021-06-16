1336f4f297911fcab3b745d53d7b38d328cd63e2
Zak <zak.rodionov@ya.ru>
2021-06-16 13:03:51 +0300

    refactor Interceptor

--- app/src/main/java/com/zakrodionov/practicalapp/app/environment/interceptors/RequestHeaderInterceptor.kt
+++ app/src/main/java/com/zakrodionov/practicalapp/app/environment/interceptors/RequestHeaderInterceptor.kt
@@ -1,24 +1,19 @@
 package com.zakrodionov.practicalapp.app.environment.interceptors
 
 import com.zakrodionov.common.extensions.headerBearer
+import com.zakrodionov.common.net.RequestInterceptor
 import com.zakrodionov.practicalapp.app.environment.preferences.AppPreferences
-import okhttp3.Interceptor
-import okhttp3.Response
+import okhttp3.Request
 
 class RequestHeaderInterceptor(
-    private val appPreferences: AppPreferences
-) : Interceptor {
+    private val appPreferences: AppPreferences,
+) : RequestInterceptor() {
 
-    override fun intercept(chain: Interceptor.Chain): Response {
-        val newRequest = chain.request()
-            .newBuilder()
-            .header("app-id", "606b5b499129d5822c058cd0") // Todo Test api
-            .apply {
-                if (appPreferences.accessToken.isNotBlank()) {
-                    headerBearer(appPreferences.accessToken)
-                }
-            }
-            .build()
-        return chain.proceed(newRequest)
+    override fun interceptRequest(requestBuilder: Request.Builder) {
+        requestBuilder.header("app-id", "606b5b499129d5822c058cd0") // Todo Test api
+
+        if (appPreferences.accessToken.isNotBlank()) {
+            requestBuilder.headerBearer(appPreferences.accessToken)
+        }
     }
 }
---
+++ common/src/main/java/com/zakrodionov/common/net/RequestInterceptor.kt
@@ -0,0 +1,20 @@
+package com.zakrodionov.common.net
+
+import okhttp3.Interceptor
+import okhttp3.Request
+import okhttp3.Response
+
+abstract class RequestInterceptor : Interceptor {
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val newRequest = chain.request()
+            .newBuilder()
+            .apply {
+                interceptRequest(this)
+            }
+            .build()
+        return chain.proceed(newRequest)
+    }
+
+    abstract fun interceptRequest(requestBuilder: Request.Builder)
+}
