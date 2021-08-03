b40d0c2bf4bffbd026b8c5f7a3b5145560e9f368
Zak <zak.rodionov@ya.ru>
2021-08-03 11:22:15 +0300

    fix ime margin

--- app/src/main/java/com/zakrodionov/practicalapp/app/features/login/ui/password/PasswordFragment.kt
+++ app/src/main/java/com/zakrodionov/practicalapp/app/features/login/ui/password/PasswordFragment.kt
@@ -3,6 +3,7 @@ package com.zakrodionov.practicalapp.app.features.login.ui.password
 import android.os.Bundle
 import android.view.View
 import by.kirich1409.viewbindingdelegate.viewBinding
+import com.zakrodionov.common.extensions.hideKeyboard
 import com.zakrodionov.common.extensions.showKeyboard
 import com.zakrodionov.common.extensions.textString
 import com.zakrodionov.practicalapp.R
@@ -22,6 +23,7 @@ class PasswordFragment : StubFragment(R.layout.fragment_password) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.btnNext.setOnClickListener {
+            binding.etPassword.hideKeyboard()
             get<AppPreferences>().isLogged = true
             viewModel.finish()
         }
