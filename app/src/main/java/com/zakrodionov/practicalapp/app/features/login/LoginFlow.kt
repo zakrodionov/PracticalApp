599e822431fe9b5357641d92e7569f9365a1cf77
Zak <zak.rodionov@ya.ru>
2021-08-23 10:53:43 +0300

    add flow

--- app/src/main/java/com/zakrodionov/practicalapp/app/features/login/LoginFlow.kt
+++ app/src/main/java/com/zakrodionov/practicalapp/app/features/login/LoginFlow.kt
@@ -1,8 +1,8 @@
 package com.zakrodionov.practicalapp.app.features.login
 
 import androidx.compose.runtime.Composable
+import com.zakrodionov.practicalapp.app.core.navigation.Flow
 import com.zakrodionov.practicalapp.app.features.login.phone.PhoneScreen
-import com.zakrodionov.practicalapp.app.ui.components.Flow
 import com.zakrodionov.practicalapp.app.ui.components.FlowContent
 
 class LoginFlow : Flow {
