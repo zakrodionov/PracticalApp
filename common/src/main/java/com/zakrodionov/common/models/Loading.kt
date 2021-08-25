b39d89d3eb2e06580a3cff1dc23a2f0b37fc4b74
Zak <zak.rodionov@ya.ru>
2021-08-25 17:24:09 +0300

    cleanup

--- common/src/main/java/com/zakrodionov/common/models/Loading.kt
+++ common/src/main/java/com/zakrodionov/common/models/Loading.kt
@@ -3,8 +3,8 @@ package com.zakrodionov.common.models
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
-@Parcelize
 // Небольшой класс для помощи обработки загрузки с SwipeRefreshLayout
+@Parcelize
 data class Loading(
     val isLoading: Boolean = false,
     val fromSwipeRefresh: Boolean = false,
