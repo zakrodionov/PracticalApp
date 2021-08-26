a106e27cad591c44298bd4411f434525ba3932b0
Zak <zak.rodionov@ya.ru>
2021-08-26 13:41:56 +0300

    add popRoot()

--- app/src/main/java/com/zakrodionov/practicalapp/app/core/navigation/NavigationExtensions.kt
+++ app/src/main/java/com/zakrodionov/practicalapp/app/core/navigation/NavigationExtensions.kt
@@ -3,4 +3,4 @@ package com.zakrodionov.practicalapp.app.core.navigation
 import cafe.adriel.voyager.navigator.Navigator
 
 // Удобно использовать во вложенной навигации(или Flow) для закрытия всей вложенной навигации
-fun Navigator.popRoot() = parent?.pop() ?: throw IllegalStateException("Parent navigator is null")
+fun Navigator.popRoot() = parent?.pop() ?: popUntilRoot()
