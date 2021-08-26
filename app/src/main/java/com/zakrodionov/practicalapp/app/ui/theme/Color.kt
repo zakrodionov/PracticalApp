56c7c66eb60afaff83726f30eba475a74437893c
Zak <zak.rodionov@ya.ru>
2021-08-26 11:19:03 +0300

    add RandomColor

--- app/src/main/java/com/zakrodionov/practicalapp/app/ui/theme/Color.kt
+++ app/src/main/java/com/zakrodionov/practicalapp/app/ui/theme/Color.kt
@@ -1,10 +1,15 @@
 @file:Suppress("MagicNumber")
+
 package com.zakrodionov.practicalapp.app.ui.theme
 
 import androidx.compose.ui.graphics.Color
+import kotlin.random.Random
 
 val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
 val StatusBarColor = Color(0xFFA8DADC)
+
+val RandomColor
+    get() = Color(Random.nextInt(256), Random.nextInt(256), Random.nextInt(256))
