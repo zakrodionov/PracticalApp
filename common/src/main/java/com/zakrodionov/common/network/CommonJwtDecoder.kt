b5229eb63743bbf5e3a9304559cddc4e24d02853
Zak <zak.rodionov@ya.ru>
2021-12-03 10:03:39 +0300

    Refactoring jwt

---
+++ common/src/main/java/com/zakrodionov/common/network/CommonJwtDecoder.kt
@@ -0,0 +1,26 @@
+package com.zakrodionov.common.network
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+import com.squareup.moshi.Moshi
+
+class CommonJwtDecoder(private val moshi: Moshi) : JwtDecoder() {
+
+    private fun getJwt(jwt: String): JwtPayload? =
+        moshi.adapter(JwtPayload::class.java).fromJson(getPayload(jwt))
+
+}
+
+@JsonClass(generateAdapter = true)
+data class JwtPayload(
+    @Json(name = "exp")
+    val exp: Long?,
+    @Json(name = "iat")
+    val iat: Long?,
+    @Json(name = "jti")
+    val jti: String?,
+    @Json(name = "typ")
+    val typ: Long?,
+    @Json(name = "uid")
+    val uid: Long?,
+)
--- common/src/main/java/com/zakrodionov/common/network/JwtDecoder.kt
+++ common/src/main/java/com/zakrodionov/common/network/JwtDecoder.kt
@@ -1,53 +1,24 @@
 package com.zakrodionov.common.network
 
 import android.util.Base64
-import com.squareup.moshi.Json
-import com.squareup.moshi.JsonClass
-import com.squareup.moshi.Moshi
-import timber.log.Timber
-import java.io.UnsupportedEncodingException
 
-class JwtDecoder(private val moshi: Moshi) {
 
-    fun getUserId(jwt: String): String = getJwtBody(jwt)?.uid?.toString() ?: ""
+abstract class JwtDecoder {
 
-    private fun getJwtBody(jwt: String): JwtBody? =
-        moshi.adapter(JwtBody::class.java).fromJson(getJsonBody(jwt))
+    protected fun getHeader(jwt: String): String = splitJwt(jwt)[0].decodeBase64String()
 
-    private fun getJsonBody(jwt: String) = getJson(decoded(jwt)[1])
+    protected fun getPayload(jwt: String): String = splitJwt(jwt)[1].decodeBase64String()
 
-    @Throws(Exception::class)
-    private fun decoded(jwt: String): Array<String> {
-        try {
-            val split = jwt.split("\\.".toRegex()).toTypedArray()
-            return split
-        } catch (e: UnsupportedEncodingException) {
-            Timber.e(e)
-            throw e
-        }
-    }
+    private fun splitJwt(jwt: String): Array<String> = jwt.split("\\.".toRegex()).toTypedArray()
 
-    @Throws(UnsupportedEncodingException::class)
-    private fun getJson(strEncoded: String): String {
-        val decodedBytes: ByteArray = Base64.decode(strEncoded, Base64.URL_SAFE)
+    private fun String.decodeBase64String(): String {
+        val decodedBytes: ByteArray = Base64.decode(this, Base64.URL_SAFE)
         return String(decodedBytes, charset("UTF-8"))
     }
 
-    @JsonClass(generateAdapter = true)
-    data class JwtBody(
-        @Json(name = "exp")
-        val exp: Long?,
-        @Json(name = "iat")
-        val iat: Long?,
-        @Json(name = "jti")
-        val jti: String?,
-        @Json(name = "new")
-        val new: Boolean?,
-        @Json(name = "priv")
-        val priv: List<String?>?,
-        @Json(name = "typ")
-        val typ: Long?,
-        @Json(name = "uid")
-        val uid: Long?
-    )
 }
+
+
+
+
+
