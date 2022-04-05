63475f5347862eed6fac51159a5aeb2c999041c3
Zak <zakrodionov@gmail.com>
2022-04-05 13:06:17 +0400

    Enable native support Apple silicon

--- buildSrc/src/main/java/Plugins.kt
+++ buildSrc/src/main/java/Plugins.kt
@@ -1,5 +1,5 @@
 object Plugins {
-    const val android_gradle_plugin = "com.android.tools.build:gradle:7.0.4"
+    const val android_gradle_plugin = "com.android.tools.build:gradle:7.1.2"
     const val kotlin_gradle_plugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlinVersion}"
     const val junit5_plugin = "de.mannodermaus.gradle.plugins:android-junit5:1.8.2.0"
     const val detekt_plugin = "io.gitlab.arturbosch.detekt"
