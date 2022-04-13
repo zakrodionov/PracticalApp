fc75e14164d70833eb668f122ef958105c54badb
Zak <zakrodionov@gmail.com>
2022-04-13 12:51:08 +0400

    Add preview for CommonCenteredButton

--- app/src/main/java/com/zakrodionov/practicalapp/app/ui/components/CommonCenteredButton.kt
+++ app/src/main/java/com/zakrodionov/practicalapp/app/ui/components/CommonCenteredButton.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -17,3 +18,9 @@ fun CommonCenteredButton(text: String, onClick: () -> Unit) =
     ) {
         CommonCenteredText(text = text)
     }
+
+@Preview
+@Composable
+fun PreviewCommonCenteredButton() {
+    CommonCenteredButton("Done") {}
+}
